5bf652ad2297ea56c1b60ab76b6a577c3a7dad37
Wanderson Pereira <wanderson.pereira@infracommerce.com.br>
2022-07-06 19:33:04 -0300

    Resolução do exercicio 12

--- task12.js
+++ task12.js
@@ -8,3 +8,97 @@
 // Como regra de negócio da classe escola, deverá filtrar todos os alunos que tiveram a média maior que 80
 
 // Imprimir na tela os nomes dos alunos que obtiveram uma média maior que 80
+
+class Materia {
+    id;
+    nome;
+    nota;
+    constructor (nomeMateria, notaMateria){
+        this.nome = nomeMateria;
+        this.nota = notaMateria;
+        this.id = new Date().getTime();
+    }
+}
+
+class Aluno {
+    id;
+    nome;
+    idade;
+    constructor (nomeAluno, idadeAluno) {
+        this.nome = nomeAluno;
+        this.idade = idadeAluno;
+        this.id = new Date().getTime();
+    }
+}
+
+class Semestre {
+    id;
+    aluno;
+    materias;
+    semestre;
+    constructor (alunoSemestre, materiasSemestre, semestreSemestre) {
+        this.aluno = alunoSemestre;
+        this.materias = materiasSemestre || [];
+        this.semestre = semestreSemestre;
+        this.id = new Date().getTime();
+    }
+
+    calcularMedia ( ){
+        var soma = 0;
+        this.materias.forEach(
+            function(materia, index){
+                soma = soma + materia.nota;
+            }
+        )
+        return soma / this.materias.length;
+    }
+}
+class Escola {
+    semestres;
+    constructor (semestreEscola) {
+        this.semestres = semestreEscola || [];
+    }
+    filtrarAluno () {
+        var resultadoFiltro = this.semestres.filter(
+            function(semestre, index) {
+                if (semestre.calcularMedia() > 80) {
+                    return true;
+                }
+                return false;
+            }
+        )
+        return resultadoFiltro;
+    }
+ }
+
+ function teste () {
+    const portugues1 = new Materia ("Português", 60);
+    const matematica1 = new Materia ("Matemática", 50);
+    const historia1 = new Materia ("Historia", 80);
+    const portugues2 = new Materia ("Português", 90);
+    const matematica2 = new Materia ("Matemática", 92);
+    const historia2 = new Materia ("História", 80);
+    
+    const angel = new Aluno ("Angel", 18);
+    const eduardo = new Aluno ("Eduardo", 29);
+    const wanderson = new Aluno ("Wanderson", 32);
+    const listaMateriasAngel = [portugues1, matematica1, historia1];
+    const listaMateriasEduardo = [portugues2, matematica2, historia2];
+
+    const semestreAngel = new Semestre (angel, listaMateriasAngel, "semestre1");
+    const semestreEduardo = new Semestre (eduardo, listaMateriasEduardo, "semestre1");
+    const semestreWanderson = new Semestre (wanderson);
+
+    const escola = new Escola([semestreAngel, semestreEduardo, semestreWanderson]);
+
+    const acima80 = escola.filtrarAluno();
+    const nomesAlunos = acima80.map(
+        function(item, index) {
+            return item.aluno.nome;
+        }
+    )
+
+    console.log(nomesAlunos);
+ }
+
+ teste();
